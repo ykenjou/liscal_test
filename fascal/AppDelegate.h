cbb09bdbfb1d888ee7e4b4425fe2b40a2d52056f
kenjou yutaka <kenjou@kenjou-no-iMac.local>
2014-01-31 08:46:46 +0900

    Initial Commit

---
+++ fascal/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  fascal
+//
+//  Created by kenjou yutaka on 2014/01/31.
+//  Copyright (c) 2014年 kenjou yutaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ fascalTests/fascalTests.m
@@ -0,0 +1,34 @@
+//
+//  fascalTests.m
+//  fascalTests
+//
+//  Created by kenjou yutaka on 2014/01/31.
+//  Copyright (c) 2014年 kenjou yutaka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fascalTests : XCTestCase
+
+@end
+
+@implementation fascalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
